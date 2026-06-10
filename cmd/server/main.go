199a636d8d7e96073c8f89895d9a396749561f46
pranav718 <raypranav718@gmail.com>
2026-06-10 23:01:17 +0530

    cli and logging structures

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,8 +2,25 @@ package main
 
 import (
 	"fmt"
+	"context"
+	"flag"
+	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/pranav718/tobira/internal/api"
 )
 
 func main() {
-	fmt.Println("initialising tobira")
+	port:= flag.String("port", "8080", "HTTP server port")
+	nodeID:= flag.String("id", "node-1", "unique node identifier")
+	flag.Parse()
+
+	logger:= slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{ Level: slog.LevelDebug}))
+	slog.SetDefault(logger);
+	slog.Info("tobira starting", "node", *nodeID, "port", *port)
+
+	
+	
 }
