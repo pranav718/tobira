54d7a39f97e6bf3b7e3717904a4df01ae1434041
pranav718 <raypranav718@gmail.com>
2026-06-11 21:27:40 +0530

    implemented fixed window algorithm

--- internal/limiter/fixed.go
+++ internal/limiter/fixed.go
@@ -25,3 +25,34 @@ func NewFixedWindow(cfg Config) *FixedWindow {
 		windows: make(map[string]*window),
 	}
 }
+
+func (fw *FixedWindow) Allow(key string) bool {
+	fw.mu.Lock()
+	defer fw.mu.Unlock()
+
+	now:= time.Now()
+	w, exists:= fw.windows[key]
+
+	if !exists || now.After(w.expiry) {
+		fw.windows[key] = &window{ count: 1, expiry: now.Add(fw.duration)}
+		slog.Debug("limiter: new window", 
+		"key", key, 
+		"count", 1, 
+		"expires", fw.windows[key].expiry.Format(time.RFC3339),
+		)
+		return true
+	}
+
+	if w.count < fw.rate {
+		w.count++
+		slog.Debug("limiter: allowed",
+			"key", key,
+			"count", w.count,
+			"limit", fw.rate,
+		)
+		return true
+	}
+
+	slog.Debug("limiter: denied", "key", key, "count", w.count, "limit", fw.rate)
+	return false
+}
