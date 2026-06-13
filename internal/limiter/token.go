0c92e16882ce2430aeb52ad38113abf224c7458d
pranav718 <raypranav718@gmail.com>
2026-06-13 13:25:15 +0530

    implemented token bucket algorithm

--- internal/limiter/token.go
+++ internal/limiter/token.go
@@ -26,3 +26,49 @@ func NewTokenBucket(cfg Config) *TokenBucket{
         buckets: make(map[string]*bucket),
 	}
 }
+
+func (tb *TokenBucket) Allow(key string) bool {
+	tb.mu.Lock()
+	defer tb.mu.Unlock()
+
+	now:= time.Now()
+	b, exists:= tb.buckets[key]
+
+	if !exists{
+		tb.buckets[key] = &bucket{
+			tokens: tb.capacity - 1,
+			lastTime: now,
+		}
+		slog.Debug("limiter: allowed",
+			"algo", "token_bucket",
+			"key", key,
+			"tokens", tb.capacity-1,
+		)
+		return true
+	}
+
+	elapsed:= now.Sub(b.lastTime).Seconds()
+	b.tokens += elapsed * tb.refillRate
+	if b.tokens > tb.capacity{
+		b.tokens = tb.capacity
+	}
+	b.lastTime = now
+
+	if b.tokens >=1 {
+		b.tokens--
+		slog.Debug("limiter: allowed",
+			"algo","token_bucket",
+			"key", key,
+			"tokens", b.tokens,
+		)
+		return true
+	}
+
+	slog.Debug("limiter: denied",
+		"algo", "token_bucket",
+		"key", key,
+		"tokens", b.tokens,
+	)
+
+	return false
+}
