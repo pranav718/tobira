c628cff4d554f5373478eedd6830bccd2b7d6c4f
pranav718 <raypranav718@gmail.com>
2026-06-15 01:38:11 +0530

    algorithm selection cli flag

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -18,6 +18,7 @@ func main() {
 	nodeID:= flag.String("id", "node-1", "unique node identifier")
 	rate := flag.Int("rate", 10 ,"max reqs per window")
 	window:= flag.Int("window", 60, "rate limiter window in seconds")
+	algorithm:= flag.String("algorithm","fixed_window","rate limit algorithm: fixed_window, sliding_window, token_bucket, leaky_bucket")
 	flag.Parse()
 
 	logger:= slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{ Level: slog.LevelDebug}))
@@ -27,26 +28,34 @@ func main() {
 	ctx, stop:= signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
 
-	lim:= limiter.NewFixedWindow(limiter.Config{ Rate: *rate, WindowSeconds: *window})
-	
-	srv:= api.NewServer(*nodeID, *port, lim)
+	lim, err := limiter.New(limiter.Config{
+		Algorithm:     *algorithm,
+		Rate:          *rate,
+		WindowSeconds: *window,
+	})
+
+	if err != nil {
+		slog.Error("failed to create limiter", "err", err)
+		os.Exit(1)
+	}
+
+	srv := api.NewServer(*nodeID, *port, lim)
 
 	go func() {
-		if err:= srv.Start(); err!=nil {
+		if err := srv.Start(); err != nil {
 			slog.Error("server error", "err", err)
 			os.Exit(1)
 		}
 	}()
 
 	slog.Info("tobira ready", "addr", fmt.Sprintf("http://localhost:%s", *port))
-
+	
 	<-ctx.Done()
-	slog.Info("shutting down :[ ")
+	slog.Info("shutting down :[")
 
-	if err:=srv.Shutdown(); err!=nil{
+	if err := srv.Shutdown(); err != nil {
 		slog.Error("shutdown error", "err", err)
 		os.Exit(1)
 	}
-
-	slog.Info("tobira stopped, gate closed :D ");
+	slog.Info("tobira stopped, gate closed :D")
 }
