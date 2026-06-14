8e70bc9fbd9c4bba4643be80c8f7bcde3e3d2fed
pranav718 <raypranav718@gmail.com>
2026-06-15 01:24:13 +0530

    implemented leaky bucket algorithm

--- internal/limiter/leaky.go
+++ internal/limiter/leaky.go
@@ -26,3 +26,43 @@ func NewLeakyBucket(cfg Config) *LeakyBucket {
 		buckets: make(map[string]*leak),
 	}
 }
+
+func (lb *LeakyBucket) Allow(key string) bool {
+	lb.mu.Lock()
+	defer lb.mu.Unlock()
+	now := time.Now()
+	l, exists := lb.buckets[key]
+	if !exists {
+		lb.buckets[key] = &leak{
+			water:    1, 
+			lastTime: now,
+		}
+		slog.Debug("limiter: allowed",
+			"algo", "leaky_bucket",
+			"key", key,
+			"water", 1.0,
+		)
+		return true
+	}
+	elapsed := now.Sub(l.lastTime).Seconds()
+	l.water -= elapsed * lb.leakRate
+	if l.water < 0 {
+		l.water = 0
+	}
+	l.lastTime = now
+	if l.water+1 <= lb.capacity {
+		l.water++
+		slog.Debug("limiter: allowed",
+			"algo", "leaky_bucket",
+			"key", key,
+			"water", l.water,
+		)
+		return true
+	}
+	slog.Debug("limiter: denied",
+		"algo", "leaky_bucket",
+		"key", key,
+		"water", l.water,
+	)
+	return false
+}
