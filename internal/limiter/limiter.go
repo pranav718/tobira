7d8d44308d42984bb1132530dc8591fc3ac4e931
pranav718 <raypranav718@gmail.com>
2026-06-16 21:11:55 +0530

    finalize algorithms and config structure

--- internal/limiter/limiter.go
+++ internal/limiter/limiter.go
@@ -1,10 +1,28 @@
 package limiter
 
+import "fmt"
+
 type Limiter interface {
 	Allow(key string) bool
 }
 
 type Config struct {
+	Algorithm string
 	Rate int
 	WindowSeconds int
+}
+
+func New(cfg Config) (Limiter, error) {
+	switch cfg.Algorithm{
+	case "fixed_window":
+		return NewFixedWindow(cfg), nil
+	case "sliding_window"
+		return NewSlidingWindow(cfg), nil
+	case "token_bucket":
+		return NewTokenBucket(cfg), nil
+	case "leaky_bucket":
+		return NewLeakyBucket(cfg), nil
+	default:
+		return nil, fmt.Error("unknown algorithm: %s", cfg.Algorithm)	
+	}
 }
